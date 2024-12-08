0473addbd6c94a53eb10a20eb31f3a7d584a8d81
Isaak <isaac@thesheridans.email>
2024-12-08 16:24:12 -0500

    started work on details page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,32 +1,37 @@
-import Image from "next/image";
-import SearchForm from "../../components/SearchForm";
-import StartupCard, {StartupTypeCard} from "@/components/StartupCard";
+import SearchForm from "@/components/SearchForm";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import { STARTUP_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { auth } from "@/auth";
 
-export default async function Home({searchParams}: {
-  searchParams: Promise<{ query?: string}>
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
 }) {
-
   const query = (await searchParams).query;
- 
   const params = { search: query || null };
 
-  const {data: posts} = await sanityFetch({query: STARTUP_QUERY, params})
+  const session = await auth();
 
-  console.log(JSON.stringify(posts, null, 2))
+  console.log(session?.id);
 
+  const { data: posts } = await sanityFetch({ query: STARTUP_QUERY, params });
 
   return (
     <>
       <section className="pink_container">
-        <h1 className="heading">Pitch Your Startup Idea, <br/>Connect With Entrapreneurs</h1>
+        <h1 className="heading">
+          Pitch Your Startup, <br />
+          Connect With Entrepreneurs
+        </h1>
 
-      <p className="sub-heading !max-w-3xl">
-        Submit Ideas, Vote on Pitches, and Make Your Projects Into Products
-      </p>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
 
-      <SearchForm query={query} />
+        <SearchForm query={query} />
       </section>
 
       <section className="section_container">
@@ -34,20 +39,18 @@ export default async function Home({searchParams}: {
           {query ? `Search results for "${query}"` : "All Startups"}
         </p>
 
-        <ul className="mt-7 card-grid">
+        <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupTypeCard, index: number) => (
-              <StartupCard key={post?._id} post={post}/>
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
             ))
-            ) : (
-             <p className="no-results">No Matches Found</p>
+          ) : (
+            <p className="no-results">No startups found</p>
           )}
         </ul>
-
       </section>
 
       <SanityLive />
     </>
-
   );
 }
